33d2cbf82978a5d2deec6269be4dc193186ed65c
CalIne2 <93724072+CalIne2@users.noreply.github.com>
2022-12-11 09:44:21 +0100

    Update create.sql

--- milestone_2/create.sql
+++ milestone_2/create.sql
@@ -9,7 +9,8 @@ create table schueler(
   nachname varchar(80) not null, 
   vorname varchar(80) not null, 
   jahrgang integer not null, 
-  geschlecht varchar(20) not null
+  geschlecht varchar(20) not null,
+  foreign key (name)
   );
   
 create table sportliche_leistung(
@@ -17,21 +18,32 @@ create table sportliche_leistung(
   km integer, 
   sportart varchar(80), 
   co2_aquivalenz integer
+  foreign key (name)
   );
   
 create table sportlehrperson(
   pers_no integer not null unique primary key, 
   nachname varchar(80), 
   vorname varchar(80)
+  foreign key (schul_nr)
   );
   
 create table zeit(
   log_no integer not null unique primary key, 
-  datum ???, 
-  uhrzeit ???
+  datum date, 
+  uhrzeit time
   );
   
 create table klasse(
   name varchar(80) not null primary key
   );
   
+create table erbringt(
+  primary key, foreign key1(schueler_nr)
+  primary key, foreign key2(count_no)
+  );
+
+create table erfolgt(
+  primary key, foreign key1(log_no)
+  primary key, foreign key2(count_no)
+  );
