30419dfc4b9ef14f713a21fda1e55efcad46145a
CalIne2 <93724072+CalIne2@users.noreply.github.com>
2022-12-11 09:19:46 +0100

    first draft

--- milestone_2/create.sql
+++ milestone_2/create.sql
@@ -1,1 +1,37 @@
+create table schule(
+  schul_nr integer not null unique primary key,
+  name varchar(80) not null, 
+  kanton varchar(80) not null
+  );
 
+create table schueler(
+  schueler_nr integer not null unique primary key, 
+  nachname varchar(80) not null, 
+  vorname varchar(80) not null, 
+  jahrgang integer not null, 
+  geschlecht varchar(20) not null
+  );
+  
+create table sportliche_leistung(
+  count_no integer not null unique primary key, 
+  km integer, 
+  sportart varchar(80), 
+  co2_aquivalenz integer
+  );
+  
+create table sportlehrperson(
+  pers_no integer not null unique primary key, 
+  nachname varchar(80), 
+  vorname varchar(80)
+  );
+  
+create table zeit(
+  log_no integer not null unique primary key, 
+  datum ???, 
+  uhrzeit ???
+  );
+  
+create table klasse(
+  name varchar(80) not null primary key
+  );
+  
