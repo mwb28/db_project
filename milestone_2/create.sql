d354267f26e7ad15571d5203bb91a83154ec7e40
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-13 19:02:33 +0100

    add check

--- milestone_2/create.sql
+++ milestone_2/create.sql
@@ -8,9 +8,11 @@ create table schueler(
   schueler_nr integer not null unique primary key, 
   nachname varchar(30) not null, 
   vorname varchar(30) not null, 
-  jahrgang integer not null,
-  geschlecht varchar(1) not null,
-  klassen_name varchar(30) not null on delete cascade,
+  Geburtsjahr year not null
+  check (Geburtsjahr > 2000 and Geburtsjahr < 2010),
+  geschlecht varchar(1) not null
+  check (geschlecht = 'm' or geschlecht = 'w' or geschlecht = 'd'),
+  klassen_name varchar(30) not null on update cascade on delete set null,
   foreign key (klassen_name)
   references klasse(name)
   );
@@ -36,13 +38,15 @@ create table sportlehrperson(
   
 create table zeit(
   log_no integer not null unique primary key, 
-  datum date, 
+  datum date
+  check (datum > '2023-01-01'), 
   uhrzeit time
+  check (uhrzeit > '00:00:00' and uhrzeit < '23:59:59')
   );
   
 create table klasse(
   name varchar(30) not null primary key,
-  sport_gehalten_von integer references sportlehrperson(pers_no)
+  sport_gehalten_von integer references sportlehrperson(pers_no) on update cascade on delete set null
   foreign key (schul_nr, pers_no)
   references sportlehrperson(schul_nr, pers_no)
   );
