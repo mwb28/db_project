03c78606bd304399a8231f2fdd5de6ee33197b97
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-14 10:10:43 +0100

    add comment

--- milestone_2/create.sql
+++ milestone_2/create.sql
@@ -1,8 +1,31 @@
+/*
+Zur Erstellung der Datenbank:
+Wir haben uns entschieden, dass wir die Datenbank in 7 Tabellen aufteilen.
+Die Tabellen sind:
+
+1. schule
+2. schueler
+3. sportliche_leistung
+4. sportlehrperson
+5. erfolgt (hier werden die Zeiten gespeichert, wann die Sportliche Leistung stattgefunden haben)
+6. klasse
+7. erbringt (hier werden die Beziehungen zwischen den Tabellen schueler und sportliche_leistung gespeichert)
+
+Die Rückmeldungen des ersten Milestones haben wir berücksichtigt und die Entitäten bzw. Attribute foglendermassen angepasst:
+Damit zwischen Schule, Sportlehrperson und Klasse keine Connection Trap ensteht, haben wir der Klasse ein zusätzliches Attribut "sport_gehalten_von" hinzugefügt,
+welches auf die Tabelle sportlehrperson verweist. Hier könnte man natürlich auch eine zusätzlich Tabelle erstellen, welche alle 
+Fächer der Klasse aufliestet, da wir aber nur am Fach Sport interessiert sind, haben wir uns für diese Lösung entschieden.
+Weiter haben wir die Beziehung zwischen Klasse und Sportliche Leistung entfernt, da diese nicht notwendig ist.
+Zudem haben wir die Beziehung erfolgt mit einer N:1 Beziehung ersetzt, damit nicht mehrere Zeiten für eine Sportliche Leistung angegeben werden können.
+*/
+
+*/
 create table schule(
   schul_nr integer not null unique primary key,
   schulname varchar(40) not null, 
   kanton varchar(30) not null
   );
+
 
 create table schueler(
   schueler_nr integer not null unique primary key, 
@@ -36,7 +59,7 @@ create table sportlehrperson(
   references schule(schul_nr)
   );
   
-create table zeit(
+create table erfolgt(
   log_no integer not null unique primary key, 
   datum date
   check (datum > '2023-01-01'), 
