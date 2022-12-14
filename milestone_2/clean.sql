935fa1e4c5fcde8d0733aab444c910247c294852
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-14 15:47:39 +0100

    update clean

---
+++ milestone_2/clean.sql
@@ -0,0 +1,13 @@
+/*
+Alle Tabellen werden geöscht, falls sie bereits existieren.
+Die Tabellen werden in der Reihenfolge gelöscht, in der sie erstellt wurden.
+Die Tabelle erbringt wird am Ende nur noch Null Werte enthalten.
+*/
+
+drop table if exists schule;
+drop table if exists schueler;
+drop table if exists sportliche_leistung;
+drop table if exists sportlehrperson;
+drop table if exists erfolgt_um;
+drop table if exists sportklasse;
+drop table if exists erbringt;
