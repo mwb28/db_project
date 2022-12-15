959e43083e9a729dbe1b24c176110c56275bbb5d
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-12-15 16:23:12 +0100

    change clean order

--- milestone_2/clean.sql
+++ milestone_2/clean.sql
@@ -4,10 +4,10 @@ Die Tabellen werden in der Reihenfolge gelöscht, in der sie erstellt wurden.
 Die Tabelle erbringt wird am Ende nur noch Null Werte enthalten.
 */
 
-drop table if exists schule;
-drop table if exists schueler;
-drop table if exists sportliche_leistung;
-drop table if exists sportlehrperson;
-drop table if exists erfolgt_um;
-drop table if exists sportklasse;
 drop table if exists erbringt;
+drop table if exists sportliche_leistung;
+drop table if exists erfolgt_um;
+drop table if exists schueler;
+drop table if exists sportklasse;
+drop table if exists sportlehrperson;
+drop table if exists schule;
