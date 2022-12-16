b067c2ffa70ff67562ac11a98f509036e24e555b
CalIne2 <93724072+CalIne2@users.noreply.github.com>
2022-12-16 14:29:51 +0100

    Update load.sql

--- milestone_2/load.sql
+++ milestone_2/load.sql
@@ -11,35 +11,35 @@ insert into schule values
 
 -- Einfügen der Daten in der Tabelle sporlehrperson
 insert into sportlehrperson values 
-(201, 'Walther', 'Matthias',102),
-(202, 'Ineichen','Calissa',104),
-(203, 'Brunner','Peter',103),
-(204, 'Holzer', 'Hans',101);
+(201, 'Walther', 'Matthias'),
+(202, 'Ineichen','Calissa'),
+(203, 'Brunner','Peter'),
+(204, 'Holzer', 'Hans');
 
 -- Einfügen der Daten in der Tabelle sportklasse 
 insert into sportklasse values 
-('26a',201,102),
-('27a',202,104),
-('24a',203,103),
-('25a',204,101),
-('23a',201,102);
+('26a',102,201),
+('27a',104,202),
+('24a',103,203),
+('25a',101,204),
+('23a',102,201);
 
 -- Einfügen der Daten in die Tabelle schueler
 insert into schueler values 
 (301, 'Muster', 'Max', 2001, 'm', '26a',102),
 (302, 'Hofer', 'Erika', 2002, 'w', '26a',102),
-(303, 'Bieler', 'Hans', 2003, 'm', '27a',103),
-(304, 'Lustiger', 'Peter', 2004, 'm', '24a',104),
+(303, 'Bieler', 'Hans', 2003, 'm', '27a',104),
+(304, 'Lustiger', 'Peter', 2004, 'm', '24a',103),
 (305, 'Höfer', 'Anna', 2005, 'w', '25a',101),
 (306, 'Walther', 'Fritz', 2006, 'm', '23a',102),
 (307, 'Gruber', 'Matthias', 2007, 'm', '25a',101),
 (308, 'Berger', 'Wilma', 2008, 'w', '26a',102),
-(309, 'Streit', 'Hans', 2009, 'm', '24a',104),
-(310, 'Bockig', 'Peter', 2009, 'm', '27a',103);
+(309, 'Streit', 'Hans', 2009, 'm', '24a',103),
+(310, 'Bockig', 'Peter', 2009, 'm', '27a',104);
 
 -- Einfügen der Daten in der Tabelle erfolgt_um
 insert into erfolgt_um values 
-(401,'2023-01-10','13:30:00'),
+(401, '2023-01-10','13:30:00'),
 (402, '2023-01-11','14:15:00'),
 (403, '2023-01-12','15:00:00'),
 (404, '2023-01-13','15:45:00'),
